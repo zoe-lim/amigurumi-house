8730a9d823a96def99e045bd205e0b4e1b62bee7
zoe-lim <113603770+zoe-lim@users.noreply.github.com>
2022-09-16 00:44:30 +0800

    Delete prototype directory

--- prototype/scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-//navigation bar for all pages when device used is mobile//\
-//hide nav bar into a navbar icon//
-let mainNav = document.getElementById('js-navbar');
-let navbarIcon = document.getElementById('js-navbar-icon');
-
-navbarIcon.addEventListener('click', function () {
-    
-    mainNav.classList.toggle('active');
-});
